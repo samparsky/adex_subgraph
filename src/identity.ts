76df5699e2f3e47b83db646fe0c20bee50d3fcf0
samparsky <8148384+samparsky@users.noreply.github.com>
2020-05-18 09:10:39 +0100

    add: lastudpatetimestamp to routine & privilede

--- src/identity.ts
+++ src/identity.ts
@@ -3,7 +3,15 @@ import { IdentityPrivilege, IdentityRoutine } from '../generated/schema'
 
 export function handleLogPrivilegeChanged(event: LogPrivilegeChanged): void {
   let id = event.address.toHexString() + event.params.addr.toHexString()
-  let identity = IdentityPrivilege.load(id) || new IdentityPrivilege(id)
+  let identity = IdentityPrivilege.load(id)
+  if(!identity) {
+    identity = new IdentityPrivilege(id)
+    identity.timestamp = event.block.timestamp.toI32()
+    identity.lastUpdateTimestamp = event.block.timestamp.toI32()
+  } else {
+    identity.lastUpdateTimestamp = event.block.timestamp.toI32()
+  }
+
   identity.identity = event.address.toHexString()
   identity.address = event.params.addr
   identity.level = event.params.privLevel
@@ -13,10 +21,18 @@ export function handleLogPrivilegeChanged(event: LogPrivilegeChanged): void {
 
 export function handleLogRoutineAuth(event: LogRoutineAuth): void {
     let id = event.address.toHexString() + event.params.hash.toHexString();
-    let identity = IdentityRoutine.load(id) || new IdentityRoutine(id)
+    let identity = IdentityRoutine.load(id)
+
+    if(!identity) {
+        identity = new IdentityRoutine(id)
+        identity.timestamp = event.block.timestamp.toI32()
+        identity.lastUpdateTimestamp = event.block.timestamp.toI32()
+    } else {
+        identity.lastUpdateTimestamp = event.block.timestamp.toI32()
+    }
+
     identity.identity = event.address.toHexString()
     identity.hash = event.params.hash
     identity.authorized = event.params.authorized
-    identity.timestamp = event.block.timestamp.toI32()
     identity.save()
 }
