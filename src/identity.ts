2a06e4304f60cc745a3ff9404f217ecd44ab4055
samparsky <8148384+samparsky@users.noreply.github.com>
2020-05-18 12:57:15 +0100

    fix: update schema

--- src/identity.ts
+++ src/identity.ts
@@ -2,7 +2,7 @@ import { LogPrivilegeChanged, LogRoutineAuth } from "../generated/templates/Iden
 import { IdentityPrivilege, IdentityRoutine } from '../generated/schema'
 
 export function handleLogPrivilegeChanged(event: LogPrivilegeChanged): void {
-  let id = event.address.toHexString() + event.params.addr.toHexString()
+  let id = event.address.toHexString() + ":" + event.params.addr.toHexString()
   let identity = IdentityPrivilege.load(id)
   if(!identity) {
     identity = new IdentityPrivilege(id)
@@ -12,7 +12,7 @@ export function handleLogPrivilegeChanged(event: LogPrivilegeChanged): void {
     identity.lastUpdateTimestamp = event.block.timestamp.toI32()
   }
 
-  identity.identity = event.address.toHexString()
+  identity.identity = event.address
   identity.address = event.params.addr
   identity.level = event.params.privLevel
   identity.timestamp = event.block.timestamp.toI32()
@@ -20,7 +20,7 @@ export function handleLogPrivilegeChanged(event: LogPrivilegeChanged): void {
 }
 
 export function handleLogRoutineAuth(event: LogRoutineAuth): void {
-    let id = event.address.toHexString() + event.params.hash.toHexString();
+    let id = event.address.toHexString() + ":" + event.params.hash.toHexString();
     let identity = IdentityRoutine.load(id)
 
     if(!identity) {
@@ -31,7 +31,7 @@ export function handleLogRoutineAuth(event: LogRoutineAuth): void {
         identity.lastUpdateTimestamp = event.block.timestamp.toI32()
     }
 
-    identity.identity = event.address.toHexString()
+    identity.identity = event.address
     identity.hash = event.params.hash
     identity.authorized = event.params.authorized
     identity.save()
