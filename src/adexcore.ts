be872c7531ab8764e64610540405adb7845d4b62
samparsky <8148384+samparsky@users.noreply.github.com>
2021-02-04 14:18:37 +0200

    update: mappings and schema

--- src/adexcore.ts
+++ src/adexcore.ts
@@ -1,5 +1,5 @@
 
-import { log, ByteArray, Address, Bytes, store } from "@graphprotocol/graph-ts"
+import { log, ByteArray, Address, Bytes, store, BigInt } from "@graphprotocol/graph-ts"
 import {
     LogChannelOpen,
     LogChannelWithdraw,
@@ -20,25 +20,28 @@ export function handleLogChannelOpen (event: LogChannelOpen): void {
 }
 
 export function handleLogChannelWithdrawExpired(event: LogChannelWithdrawExpired): void {
-    let channel = new ChannelWithdrawExpired(event.transaction.hash.toHexString() + ':' + event.params.channelId.toHexString())
+    let channel = new ChannelWithdrawExpired(event.transaction.hash.toHexString() + ':' + event.params.channelId.toHexString() + ':' + event.logIndex.toString())
     channel.user = event.transaction.from
     channel.amount = event.params.amount
-    channel.channelId = event.params.channelId
+    channel.channel = event.params.channelId.toHexString()
     channel.timestamp = event.block.timestamp.toI32()
     channel.save()
 }
 
 export function handleLogChannelWithdraw(event: LogChannelWithdraw): void {
-    let channel = new ChannelWithdraw(event.transaction.hash.toHexString() + ':' + event.transaction.from.toHexString())
+    let channel = new ChannelWithdraw(event.transaction.hash.toHexString() + ':' + event.transaction.from.toHexString() + ':' + event.logIndex.toString())
     channel.user = event.transaction.from
     channel.amount = event.params.amount
-    channel.channelId = event.params.channelId
+    channel.channel = event.params.channelId.toHexString()
     channel.timestamp = event.block.timestamp.toI32()
     channel.save()
 } 
 
 export function handleChannel(call: ChannelOpenCall): void {
-    let channels = getOrInitChannel(call.transaction.hash.toHexString())
+    let intermediateChannels = getOrInitChannel(call.transaction.hash.toHexString())
+    
+    let channels = getOrInitChannel(intermediateChannels.channelId.toHexString())
+    channels.channelId = intermediateChannels.channelId
     channels.tokenAddr = call.inputs.channel.tokenAddr
     channels.creator = call.inputs.channel.creator
     channels.tokenAmount = call.inputs.channel.tokenAmount
@@ -47,4 +50,7 @@ export function handleChannel(call: ChannelOpenCall): void {
     channels.validUntil = call.inputs.channel.validUntil
     channels.spec = call.inputs.channel.spec.toHexString()
     channels.save()
+
+    // remove intermediate Channel saved
+    store.remove('Channel', call.transaction.hash.toHexString());
 }
